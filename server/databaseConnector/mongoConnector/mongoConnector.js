fe336ba3a1e9634d8544b5b7cfa805888df1c0e9
Ben-PC-2 <bc234cc@gmail.com>
2024-02-26 20:43:36 -0800

    Added in user crud for mongo connector

--- server/databaseConnector/mongoConnector/mongoConnector.js
+++ server/databaseConnector/mongoConnector/mongoConnector.js
@@ -1,30 +1,32 @@
+import { User } from '../../dataObjects/user';
 import { databaseConnector } from '../databaseConnector';
 
 const { MongoClient } = require("mongodb");
 
 class mongoDBConnector extends databaseConnector{
+  constructor(dbURL, credentials, dbName){
+    //Recipe collection map
+    this.collectionMap = {
+      'Users': 'userCollection',
+      'Ingredients': 'ingredientsCollection',
+      'Recipes': 'recipeCollection'
+    }
 
-    
-
-// Define the database connector class
-class DbConnector {
-  // Constructor
-  constructor(uri, dbName) {
-    // Store the URI and database name as instance fields
-    this.uri = uri;
-    this.dbName = dbName;
-    // Initialize the client and database as null
-    this.client = null;
-    this.db = null;
+    //set the url, login credentials
+    this.dbURL = dbURL
+    this.credentials = credentials
+    this.dbName = dbName
+    //attempt to connect to client
+    this.client = this.connect()
   }
 
   // Connect to the database
-  async connect() {
+  async connect(){
     try {
       // Create a new MongoClient instance
       this.client = new MongoClient(this.uri, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
+        tlsCertificateKeyFile: this.credentials,
+        serverApi: ServerApiVersion.v1
       });
       // Connect to the server
       await this.client.connect();
@@ -43,9 +45,6 @@ class DbConnector {
     try {
       // Close the client connection
       await this.client.close();
-      // Set the client and database to null
-      this.client = null;
-      this.db = null;
       // Log a success message
       console.log("Disconnected from the database");
     } catch (err) {
@@ -54,65 +53,89 @@ class DbConnector {
     }
   }
 
-    createUser(userToAdd) {
-        if (typeof userToAdd !== User) {
-          throw new Error("Subclasses must implement requiredMethod with valid parameters");
-        }
-      }
+  async createUser(userToAdd) {
+    let documentJSON = []
     
-      readUser(userName) {
-          /*allow for search for users by username, name, email using templated query that builds based on additions of optional args */
-          if (typeof userName !== "string") {
-            throw new Error("Subclasses must implement requiredMethod with valid parameters");
-          }
-      }
+    for (let index = 0; index < userToAdd.length; index++) {
+      documentJSON.append(userToAdd[index].toJSON())
+    }
     
-      searchUsers(userName, name) {
-  
+    try{
+      collection = this.dbName.collection(this.collectionMap['Users']) 
+      await collection.insertMany(documentJSON)
+    }catch(err){
+      console.error(err);
+    }
+  }
+
+  async readUser(userNameToFind) {
+      /*allow for search for users by username, name, email using templated query that builds based on additions of optional args */
+      try{
+        collection = this.dbName.collection(this.collectionMap['Users']) 
+        user = await collection.find({userName: userNameToFind})
+      }catch(err){
+        console.error(err);
       }
-  
-      updateUser(userName, userObject) {
-  
-      }
-    
-      deleteUser(userName, name) {
-  
-      }
-  
-      createRecipe(recipeObject) {
-  
-      }
-  
-      readRecipe(name, ingredients) {
-  
-      }
-  
-      updateRecipe(name, recipeObject) {
-  
-      }
-  
-      deleteRecipe(name) {
-  
-      }
-  
-  
-      createIngredient(ingredientObject) {
-  
-      }
-  
-      readIngredient(name, typeInfo) {
-  
-      }
-  
-      updateIngredient(name, ingredientObject) {
-  
-      }
-  
-      deleteIngredient(name) {
-  
-      }
+      return User(user)
+  }
+
+  //may need to llook at text search operator
+  async searchUsers(userName, name) {
+    //build search query
+    const query = {
+      $or: [
+        { name: { $regex: query, $options: "i" } }, // Case-insensitive
+        { username: { $regex: query, $options: "i" } },
+      ],
+    };
+    try{
+      collection = this.dbName.collection(this.collectionMap['Users']) 
+      foundUsersCursor = await collection.find({})
+    }catch(err){
+      console.error(err);
+    }
+    return User(user)
+  }
+
+  updateUser(userName, userObject) {
+
+  }
+
+  deleteUser(userName, name) {
+
+  }
+
+  createRecipe(recipeObject) {
+
+  }
+
+  readRecipe(name, ingredients) {
+
+  }
+
+  updateRecipe(name, recipeObject) {
+
+  }
+
+  deleteRecipe(name) {
+
+  }
+
+
+  createIngredient(ingredientObject) {
+
+  }
+
+  readIngredient(name, typeInfo) {
+
+  }
+
+  updateIngredient(name, ingredientObject) {
+
+  }
+
+  deleteIngredient(name) {
+
+  }
 
 }
-
-
-
